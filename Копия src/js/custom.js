c9736e7b45e0a8d9f6703aef0c2c37d0c98d2320
David Mekerishvili <paperdavid@gmail.com>
2016-01-11 22:51:15 +0300

    New features

---
+++ Копия src/js/custom.js
@@ -0,0 +1,29 @@
+/**************************************/
+/* Custom JavaScript files supervisor */
+/**************************************/
+
+$(document).ready(function() {
+
+    /* Custom */
+
+    //= ./common/google-analytics.js
+
+
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+
+});
